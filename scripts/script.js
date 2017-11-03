33d20ea5d86e9cd9617213ccb9eb7b9116566a8a
jdfirestone <31974973+jdfirestone@users.noreply.github.com>
2017-11-03 18:46:50 -0400

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $("[data-fancybox]").fancybox({
+    protect: true
+    autoScale: true
+
+      // Options will go here
+  	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); // do not delete
